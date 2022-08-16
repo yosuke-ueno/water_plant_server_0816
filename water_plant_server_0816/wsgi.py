85ce7e72feb42c92a950d588bb5a47fd857d0057
yosuke_ueno <yosuke.sos.pel@gmail.com>
2022-08-16 10:14:02 +0900

    first commit

---
+++ water_plant_server_0816/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for water_plant_server_0816 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'water_plant_server_0816.settings')
+
+application = get_wsgi_application()
