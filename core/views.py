8269daabc2f98b8556012ff89a24852e9caf68ef
yosuke-ueno <yosuke.sos.pel@gmail.com>
2022-08-18 16:36:00 +0900

    ベース作成

--- core/views.py
+++ core/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import WaterPlantsSerializer
+from .models import WaterPlants
 
-# Create your views here.
+
+class WaterPlantsViewSet(viewsets.ModelViewSet):
+    serializer_class = WaterPlantsSerializer
+    queryset = WaterPlants.objects.all()
